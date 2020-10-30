57636963ad26fed93fa97fbcac47f907d1c5d3b7
valchevgd <valchevgd@gmail.com>
2020-10-31 00:47:13 +0200

    Conference index and show pages

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,34 +2,50 @@
 
 namespace App\Controller;
 
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 class ConferenceController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
-     * @param Request $request
+     * @param Environment $twig
+     * @param ConferenceRepository $conferenceRepository
      * @return Response
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
      */
-    public function index(Request $request): Response
+    public function index(Environment $twig, ConferenceRepository $conferenceRepository)
     {
-        $greet = '';
+        return new Response($twig->render('conference/index.html.twig', [
+            'conferences' => $conferenceRepository->findAll()
+        ]));
+    }
 
-        if ($name = $request->query->get('hello')) {
-            $greet = sprintf('<h1>Hello %s</h1>', htmlspecialchars($name));
-        }
-
-        return new Response(<<<EOF
-<html>
-  <body>
-  $greet
-    <img src="/images/under-construction.gif">  
-  </body>
-</html>
-EOF
-        );
+    /**
+     * @Route("/conference/{id}", name="conference")
+     * @param Environment $twig
+     * @param Conference $conference
+     * @param CommentRepository $commentRepository
+     * @return Response
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function show(Environment $twig, Conference $conference, CommentRepository $commentRepository)
+    {
+        return new Response($twig->render('conference/show.html.twig', [
+            'conference' => $conference,
+            'comments' => $commentRepository->findBy(['conference' => $conference], ['createdAt' => 'DESC'])
+        ]));
     }
 }
